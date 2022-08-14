1eb3a5a0acecb25fcf4ae88a91e8639feb40255e
Akeva Melchor <akeva.melchor@expel.io>
2022-08-14 13:24:23 -0700

    fix saved books

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,7 +3,7 @@ import { useMutation } from '@apollo/client'
 import { SAVE_BOOK } from '../utils/mutations'
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import Auth from '../utils/auth';
-// import { saveBook, searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 
@@ -12,7 +12,7 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
+  const [saveBook, { error }] = useMutation(SAVE_BOOK)
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
@@ -50,7 +50,6 @@ const SearchBooks = () => {
       setSearchedBooks(bookData);
       setSearchInput('');
     } catch (err) {
-      console.error(err);
     }
   };
 
@@ -59,8 +58,6 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-
-    const [saveBook, { error }] = useMutation(SAVE_BOOK)
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
